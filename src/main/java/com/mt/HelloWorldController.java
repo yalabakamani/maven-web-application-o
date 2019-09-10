a0b50b6c920e484eb5d55c193b7be9c0f11d466b
Mithun Technologies <devopstrainingblr@gmail.com>
2019-09-10 19:48:11 +0530

    Update HelloWorldController.java

--- src/main/java/com/mt/HelloWorldController.java
+++ src/main/java/com/mt/HelloWorldController.java
@@ -1,20 +1,33 @@
-package com.mt;
+package com.mt.services;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
-public class HelloWorldController {
+public class EmployeeService {
 
-		@RequestMapping(method = RequestMethod.GET)
-		public String hello(ModelMap model) {
+	
+	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
+	@ResponseBody
+	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
+			throws JSONException {
 
-			model.addAttribute("name", " Hello World!");
-			return "home";
+		JSONObject js = new JSONObject();
+		js.put("Name", "Mithun Technologies");
+		js.put("Calling Name", "Mithun");
+		js.put("DOB", "08-Nov-2011");
+		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
+		js.put("Places he like", "His native place");
 
-		}
-		
-	}
+		return js.toString();
+}
+}
